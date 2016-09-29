41024316dc1a9e7d1df2ce91feea40a6d0aa4f73
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-09-29 14:18:51 +0900

    コメント箇所の修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919035702) do
+ActiveRecord::Schema.define(version: 20160929045237) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20160919035702) do
     t.string   "member_of",              limit: 255
     t.text     "profile",                limit: 65535
     t.string   "works",                  limit: 255
-    t.string   "avater",                 limit: 255
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
