9ad6fdc2a576964e67b37a6cc05c8ada9ab2ddfc
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-09-30 17:53:56 +0900

    edit:Userテーブルからインスタンス取得  update:Userテーブル更新とリダイレクトページ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,20 @@
     @user=User.find(params[:id])
    end
    def edit
+    @user=User.find(params[:id])
    end
    def update
+    current_user.update(update_params)
+    redirect_to :root and return
    end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :email, :member, :profile, :works, :avatar)
   end
+  end
+
+
+
+
+
