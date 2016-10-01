c354c06984db4cacecdba9df8d568b8e491d5f4e
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-01 20:06:37 +0900

    失敗メッセージをredirectからrenderへ変更
    '

--- app/controllers/flash_samples_controller.rb
+++ app/controllers/flash_samples_controller.rb
@@ -1,8 +1,11 @@
-def create
-  review = current_user.reviews.new(review_params)
-  if review.save
-    redirect_to books_url, success: "Successfully created your prototype."
-  else
-    redirect_to root_url,  warning: "Unfortunately failed to create."
+class Flash_samples_Controller < ApplicationController
+  def create
+    review = current_user.reviews.new(review_params)
+    if review.save
+     redirect_to books_url, success: "Successfully created your prototype."
+    else
+      flash[:warning] = "Unfortunately failed to create."
+      render action: 'root_url'
+    end
   end
 end
