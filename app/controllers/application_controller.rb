ff86efc456ad03b939cbceb6a950a21d2f69bcab
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-01 12:03:59 +0900

    フラッシュメッセージのキーを許可する記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
   class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  add_flash_types :success, :info, :warning, :danger
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :avatar, :profile, :works, :member, :password ])
   end
   end
-
