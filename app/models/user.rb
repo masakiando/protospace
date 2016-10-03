2e1a4e465adad90413ba1e978e1c735dc30e14ed
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-03 11:15:57 +0900

    AvatarUploaderの設定・  user、prototypes間のアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatables
-
+  mount_uploader :avatar, AvatarUploader
+  has_many :prototypes
 end
