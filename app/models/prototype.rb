846c60acd10fe48755c5414d04da1ea8080fa49f
Masaki Ando <masakiando@Masaki-no-MacBook-Pro.local>
2016-10-03 15:04:42 +0900

    imageデータが空の場合は空データを保存しないように設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :capturedimages
-  accepts_nested_attributes_for :capturedimages
-  
+  accepts_nested_attributes_for :capturedimages, reject_if: proc { |attributes| attributes["image"].blank? }
 
   validates_presence_of :title, :catchcopy, :concept
 end
